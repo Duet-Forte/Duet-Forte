abe8a88f73a5f8645d329f9ff692b03d1a621224
wave-sound <lunar105630@gmail.com>
2024-04-21 18:19:09 +0900

    Add : Add basic cutscene

--- Assets/Script/General/Manager/CutsceneManager.cs
+++ Assets/Script/General/Manager/CutsceneManager.cs
@@ -1,17 +1,21 @@
 using DG.Tweening;
-using System.Collections;
 using System.Collections.Generic;
-using System.Resources;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
+using UnityEngine.UI;
 
 public class CutsceneManager : MonoBehaviour
 {
     private GameObject cutscenePlayer;
     private PlayableDirector director;
     private int currentCutsceneID;
+    private bool isReachedLoopEndPoint;
+    private bool isTalking;
+    private double loopStartTime;
+    private Sequence fadeInAndOut;
     [SerializeField] private PlayableAsset[] cutscenes;
+    [SerializeField] private Image fadeOutPanel;
     private Dictionary<string, PlayableAsset> cutsceneDictionary;
 
     public void InitSettings()
@@ -22,9 +26,14 @@ public class CutsceneManager : MonoBehaviour
         {
             cutsceneDictionary.Add(cutscene.name, cutscene);
         }
-        director.playableAsset = cutscenes[0];
+        StartCutscene("TimmyHouse");
         cutscenePlayer = SceneManager.Instance.FieldManager.Field.GetCutsceneObject("CutscenePlayer");
+        GameObject pitch = SceneManager.Instance.FieldManager.Field.GetCutsceneObject("Pitch");
+        GameObject timmy = SceneManager.Instance.FieldManager.Field.GetCutsceneObject("Timmy");
         BindCutscene(director, "Player", cutscenePlayer);
+        BindCutscene(director, "Pitch", pitch);
+        BindCutscene(director, "Timmy", timmy);
+        isReachedLoopEndPoint = false;
         director.Play();
     }
     public void StartCutscene(string cutsceneName)
@@ -33,9 +42,10 @@ public class CutsceneManager : MonoBehaviour
     }
     public async void Talk()
     {
-        director.Pause();
+        isTalking = true;
         await DialogueManager.Instance.Talk("Cutscene", currentCutsceneID);
         ++currentCutsceneID;
+        isTalking = false;
         director.Play();
     }
 
@@ -44,7 +54,32 @@ public class CutsceneManager : MonoBehaviour
         cutscenePlayer.SetActive(false);
         SceneManager.Instance.FieldManager.SpawnPlayer(cutscenePlayer.transform.position);
     }
+    public void Loop()
+    {
+        if (!isTalking)
+        {
+            isReachedLoopEndPoint = false;
+            return;
+        }
 
+        if (isReachedLoopEndPoint && director.time != loopStartTime)
+            director.time = loopStartTime;
+        else
+            loopStartTime = director.time;
+
+        isReachedLoopEndPoint = true;
+    }
+    public void FadeOut()
+    {
+        if(fadeInAndOut == null)
+        {
+            Tween fadeIn = fadeOutPanel.DOFade(1, 1f);
+            Tween fadeOut = fadeOutPanel.DOFade(0, 1f);
+            fadeInAndOut = DOTween.Sequence();
+            fadeInAndOut.Append(fadeIn).Append(fadeOut);
+        }
+        fadeInAndOut.Play();
+    }
     public virtual void BindCutscene(PlayableDirector director, string trackGroupName, GameObject bindObject)
     {
         var timeline = director.playableAsset as TimelineAsset;
@@ -63,6 +98,7 @@ public class CutsceneManager : MonoBehaviour
                         director.SetGenericBinding(animationTrack, bindObject.GetComponent<Animator>());
                         break;
                     case SignalTrack signalTrack:
+
                         break;
                 }
             }
