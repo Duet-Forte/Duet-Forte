d7938458be451cb442a1f91ddb3f626e38e7b46b
wave-sound <lunar105630@gmail.com>
2024-05-10 22:39:43 +0900

    [Fix] Fix type sound speed

--- Assets/Script/TopView/Dialogue/TypeSound.cs
+++ Assets/Script/TopView/Dialogue/TypeSound.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class TypeSound : MonoBehaviour
 {
+    public int count;
     public void MakeTypeSound(char dummy)
     {
+        if (count >= 1)
+        {
+            count = 0;
+            return;
+        }
+        count++;
         AkSoundEngine.PostEvent("Ui_Typing_SFX", gameObject);   
     }
 }
