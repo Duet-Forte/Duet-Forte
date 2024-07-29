7f0e767f9d7023b424a172edf916c17fa039b15b
Jeong Jae Wook <jgy4846@naver.com>
2024-07-30 01:29:59 +0900

    :sparkles: edit offset

--- Assets/Script/UI/SkillPopUpUI.cs
+++ Assets/Script/UI/SkillPopUpUI.cs
@@ -8,17 +8,16 @@ public class SkillPopUpUI : MonoBehaviour
 {
     [SerializeField] Sprite dummySprite;
     [SerializeField] string dummyText;
-    [SerializeField] private int moveOffset;
+    [SerializeField] private float moveOffset;
     public void Appear(Sprite skillImage,string skillName) {
 
         transform.GetChild(1).GetComponent<Image>().sprite=skillImage;
         transform.GetChild(0).GetComponent<TMP_Text>().text=skillName;
-        Debug.Log($"skillPopUpUIÀÇ x°ª : {transform.position.x}");
         transform.DOMove(new Vector2(transform.position.x + moveOffset, transform.position.y), 0.2f);
 
     }
     public void Disappear() {
 
-        transform.DOMove(new Vector2(transform.position.x - moveOffset-2, transform.position.y), 0.2f);
+        transform.DOMove(new Vector2(transform.position.x - moveOffset, transform.position.y), 0.2f);
     }
 }
